fe145e78c9f9a2e85ad117b6126de9c1060ac974
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-20 16:21:02 +0100

    Página de cadrastro terminado

--- cadastro/utilizador.php
+++ cadastro/utilizador.php
@@ -29,8 +29,8 @@
 
                         <div class="col pe-4">
 
-                            <label class="w-100 text-start pt-3" for="n_bi">Nº BI</label> <br>
-                            <input type="text" class="form-control" name="n_bi" id="n_bi">
+                            <label class="w-100 text-start pt-3" for="email">E-mail</label> <br>
+                            <input type="email" class="form-control" name="email" id="email">
 
                             <label class="w-100 text-start pt-3" for="n_bi">Senha</label> <br>
                             <input type="password" class="form-control" name="senha" id="senha">
@@ -39,26 +39,35 @@
 
                         <div class="col pe-4 ">
 
-                            <label class="w-100 text-start pt-3" for="n_bi">Nº BI</label> <br>
-                            <input type="text" class="form-control" name="n_bi" id="n_bi">
-
-                            <label class="w-100 text-start pt-3" for="n_bi">Senha</label> <br>
-                            <input type="password" class="form-control" name="senha" id="senha">
+                            <label class="w-100 text-start pt-3" for="nascimento">Data de Nascimento</label> <br>
+                            <input type="date" class="form-control" name="nascimento" id="nascimento">
+                            
+                            <label class="w-100 text-start pt-3" for="genero">Gênero</label> <br>
+                            <select name="genero" id="genero">
+                                <option value="">Selecione...</option>
+                                <option value="M">Masculino</option>
+                                <option value="F">Femenino</option>
+                            </select>
 
                         </div>
 
                         <div class="col pe-4">
-
                             <label class="w-100 text-start pt-3" for="n_bi">Nº BI</label> <br>
                             <input type="text" class="form-control" name="n_bi" id="n_bi">
 
-                            <label class="w-100 text-start pt-3" for="n_bi">Senha</label> <br>
-                            <input type="password" class="form-control" name="senha" id="senha">
-
+                            <label class="w-100 text-start pt-3" for="acesso">Tipo de Acesso</label> <br>
+                            <select name="acesso" id="acesso">
+                                <option value="">Selecione...</option>
+                                <option value="gestor">Gestor</option>
+                                <option value="publicador">Publicador</option>
+                                <option value="apostador">Apostador</option>
+                                <option value="administrador">Administrador</option>
+                            </select>
                         </div>
                     </div>
 
-                    <div class="col col-md-3 mt-4 pe-4">
+                    <hr class="d-none d-md-block">
+                    <div class="col col-md-2 mt-4 pe-4">
                             <input type="submit" class="form-control" name="btn_cadastrar" id="btn_cadastrar"
                                 value="Cadastrar">
                     </div>
