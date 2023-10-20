1199a492d0fb24991464d5b186dcf222bc5fd5bc
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-20 16:45:46 +0100

    Base de dados recuperada

--- config/doc/db.sql
+++ config/doc/db.sql
@@ -2,16 +2,45 @@ create database sistema_aposta;
 
 use sistema_aposta;
 
-create table utilizador (
-    id_utilizador int primary key auto_increment not null,
-    nome_utilizador varchar(50),
-    sobrenome_utilizador varchar(50),
-    email_utilizador varchar(30),
-    senha_utilizador varchar(25),
-    nascimento_utilizador date,
-    genero_utilizador enum('M', 'F'),
-    n_bi_utilizador varchar(12),
-    acesso_utilizador enum('adiminstrador', 'gestor', 'publicador', 'apostador')
+create table gestor(
+    id_gestor int primary key auto_increment not null,
+    nome_gestor varchar(50),
+    sobrenome_gestor varchar(50),
+    email_gestor varchar(30),
+    senha_gestor varchar(25),
+    nascimento_gestor date,
+    genero_gestor enum('M', 'F'),
+    n_bi_gestor varchar(12)
+);
+create table publicador(
+    id_publicador int primary key auto_increment not null,
+    nome_publicador varchar(50),
+    sobrenome_publicador varchar(50),
+    email_publicador varchar(30),
+    senha_publicador varchar(25),
+    nascimento_publicador date,
+    genero_publicador enum('M', 'F'),
+    n_bi_publicador varchar(12)
+);
+create table apostador(
+    id_apostador int primary key auto_increment not null,
+    nome_apostador varchar(50),
+    sobrenome_apostador varchar(50),
+    email_apostador varchar(30),
+    senha_apostador varchar(25),
+    nascimento_apostador date,
+    genero_apostador enum('M', 'F'),
+    n_bi_apostador varchar(12)
+);
+create table adiminstrador(
+    id_adiminstrador int primary key auto_increment not null,
+    nome_adiminstrador varchar(50),
+    sobrenome_adiminstrador varchar(50),
+    email_adiminstrador varchar(30),
+    senha_adiminstrador varchar(25),
+    nascimento_adiminstrador date,
+    genero_adiminstrador enum('M', 'F'),
+    n_bi_adiminstrador varchar(12)
 );
 
 create table equipa(
@@ -25,8 +54,8 @@ create table partida(
     foreign key (id_equipaA) references equipa(id_equipa) on delete cascade,
     id_equipaB int,
     foreign key (id_equipaB) references equipa(id_equipa) on delete cascade,
-    id_utilizador int,
-    foreign key (id_utilizador) references utilizador(id_utilizador) on delete cascade
+    id_gestor int,
+    foreign key (id_gestor) references gestor(id_gestor) on delete cascade
 );
 
 create table partida_publicada(
@@ -37,8 +66,8 @@ create table partida_publicada(
     hora_partida time,
     data_publicada date,
     hora_publicada time,
-    id_utilizador int,
-    foreign key (id_utilizador) references utilizador(id_utilizador) on delete cascade
+    id_publicador int,
+    foreign key (id_publicador) references publicador(id_publicador) on delete cascade
 );
 
 create table resultado_publicado(
@@ -49,14 +78,14 @@ create table resultado_publicado(
     golos_equipaB int,
     data_publicada date,
     hora_publicada time,
-    id_utilizador int,
-    foreign key (id_utilizador) references utilizador(id_utilizador) on delete cascade
+    id_publicador int,
+    foreign key (id_publicador) references publicador(id_publicador) on delete cascade
 );
 
 create table aposta(
     id_aposta int primary key auto_increment not null,
-    id_utilizador int,
-    foreign key (id_utilizador) references utilizador(id_utilizador) on delete cascade,
+    id_apostador int,
+    foreign key (id_apostador) references apostador(id_apostador) on delete cascade,
     id_partida_pub int,
     foreign key (id_partida_pub) references partida_publicada(id_partida_pub) on delete cascade,
     golos_equipaA int,
