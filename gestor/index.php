47b1bd219ae07b138f2c338e881ed3f245e713e6
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-21 09:16:46 +0100

    Fazendo o Painel de actividades

--- gestor/index.php
+++ gestor/index.php
@@ -5,8 +5,50 @@
 <link rel="stylesheet" href="../assets/css/inputs.css">
 <title>Página Inicial</title>
 
-<main>
-    Nome: 
+<main class="mb-4 mt-4">
+    <div class="container border">
+        <div class="row border pt-2" style="background: khaki">
+            <h3> <i class="fas fa-futbol"></i> Painel de actividades</h3>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr>
+                            <th>Equipas</th>
+                        </tr>
+
+                        <?php 
+                            $equipa_dao = new EquipaDao();
+                            $retorno = $equipa_dao->Listar();
+                            foreach ($retorno as $value) {
+                               echo "<tr>";
+                               echo "<td>" . $value['nome_equipa'] . "</td>";
+                               echo "</tr>";
+                            }
+                        ?>
+
+                    </table>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="table-responsive">
+                    <table class="table">
+                        <tr>
+                            <td>Partidas</td>
+                        </tr>
+
+                        <tr>
+
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </main>
 
 <?php include "../inc/footer.html"; ?>
