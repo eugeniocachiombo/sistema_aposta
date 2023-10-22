8dc69926a5c9492d62f947de0a42f75f98511598
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-23 00:17:48 +0100

    única lista principal criada

--- apostador/listas_menu/index.php
+++ apostador/listas_menu/index.php
@@ -17,30 +17,5 @@
         </ul>
     </li>
 
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-            aria-expanded="false">
-            Equipa
-        </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="../equipa/cadastrar.php">Cadastrar</a></li>
-            <li><a class="dropdown-item" href="../equipa/actualizar.php">Actualizar</a></li>
-            <li><a class="dropdown-item" href="../equipa/listar.php">Listar</a></li>
-            <li><a class="dropdown-item" href="../equipa/eliminar.php">Eliminar</a></li>
-        </ul>
-    </li>
-
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-            aria-expanded="false">
-            Partida
-        </a>
-        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="../partida/listar.php">Listar</a></li>
-        </ul>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="../terminar_sessao">Terminar Sessão</a>
-    </li>
+    <?php include "../lista_principal_menu/index.php"; ?>
 </ul>
---
+++ lista_principal_menu/index.php
@@ -0,0 +1,33 @@
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+        aria-expanded="false">
+        Equipa
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <?php if( $acesso == "gestor" || $acesso == "administrador" ){ ?>
+        <li><a class="dropdown-item" href="../equipa/cadastrar.php">Cadastrar</a></li>
+        <li><a class="dropdown-item" href="../equipa/actualizar.php">Actualizar</a></li>
+        <li><a class="dropdown-item" href="../equipa/eliminar.php">Eliminar</a></li>
+        <?php } ?>
+        <li><a class="dropdown-item" href="../equipa/listar.php">Listar</a></li>
+    </ul>
+</li>
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+        aria-expanded="false">
+        Partida
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <?php if( $acesso == "gestor" || $acesso == "administrador" ){ ?>
+        <li><a class="dropdown-item" href="../partida/cadastrar.php">Criar</a></li>
+        <li><a class="dropdown-item" href="../partida/actualizar.php">Actualizar</a></li>
+        <li><a class="dropdown-item" href="../partida/eliminar.php">Eliminar</a></li>
+        <?php } ?>
+        <li><a class="dropdown-item" href="../partida/listar.php">Listar</a></li>
+    </ul>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href="../terminar_sessao">Terminar Sessão</a>
+</li>
