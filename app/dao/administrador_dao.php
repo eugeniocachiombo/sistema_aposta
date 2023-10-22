0a3ea5f8b246a752228afd96173cad2f19db639e
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-22 21:50:01 +0100

    Publicador dao e administrador dao resolvidos

--- app/dao/administrador_dao.php
+++ app/dao/administrador_dao.php
@@ -4,7 +4,7 @@ class AdministradorDao implements Crud {
     
     function Cadastrar($administrador){
         $con = GetConexao();
-        $sql = "insert into administrador (nome_administrador, sobrenome_administrador, email_administrador, senha_administrador, nascimento_administrador, genero_administrador, n_bi_administrador) values (?,?,?,?,?,?,?);";
+        $sql = "insert into administrador (nome_administrador, sobrenome_administrador, email_administrador, senha_administrador, nascimento_administrador, genero_administrador, n_bi_administrador) values (?, ?, ?, md5(?), ?, ?, ?);";
         $stmt = $con->prepare($sql);
         $stmt->bindValue( 1, $administrador->GetNome());
         $stmt->bindValue( 2, $administrador->GetSobrenome());
@@ -18,16 +18,24 @@ class AdministradorDao implements Crud {
 
     function Actualizar($administrador){
         $con = GetConexao();
-        $sql = "update administrador set nome_administrador = ?, sobrenome_administrador = ?, email_administrador = ?, senha_administrador = ?, nascimento_administrador = ?, genero_administrador = ?, n_bi_administrador = ? where id_administrador = ?;";
+        $sql = "update administrador set nome_administrador = ?, sobrenome_administrador = ?, email_administrador = ?, nascimento_administrador = ?, genero_administrador = ?, n_bi_administrador = ? where id_administrador = ?;";
         $stmt = $con->prepare($sql);
         $stmt->bindValue( 1, $administrador->GetNome());
         $stmt->bindValue( 2, $administrador->GetSobrenome());
         $stmt->bindValue( 3, $administrador->GetEmail());
-        $stmt->bindValue( 4, $administrador->GetSenha());
-        $stmt->bindValue( 5, $administrador->GetNascimento());
-        $stmt->bindValue( 6, $administrador->GetGenero());
-        $stmt->bindValue( 7, $administrador->GetN_bi());
-        $stmt->bindValue( 8, $administrador->GetId());
+        $stmt->bindValue( 4, $administrador->GetNascimento());
+        $stmt->bindValue( 5, $administrador->GetGenero());
+        $stmt->bindValue( 6, $administrador->GetN_bi());
+        $stmt->bindValue( 7, $administrador->GetId());
+        return $stmt->execute();
+    }
+
+    function AlterarSenha($id_administrador, $senha_nova){
+        $con = GetConexao();
+        $sql = "update administrador set senha_administrador = md5(?) where id_administrador = ?;";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue( 1, $senha_nova);
+        $stmt->bindValue( 2, $id_administrador);
         return $stmt->execute();
     }
 
@@ -43,7 +51,8 @@ class AdministradorDao implements Crud {
         $con = GetConexao();
         $sql = "select * from administrador;";
         $stmt = $con->prepare($sql);
-        return $stmt->execute();
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
 
     function ListarPorId($id){
@@ -51,7 +60,38 @@ class AdministradorDao implements Crud {
         $sql = "select * from administrador where id_administrador = ?;";
         $stmt = $con->prepare($sql);
         $stmt->bindValue( 1, $id);
-        return $stmt->execute();
+        $stmt->execute();
+        return $stmt->fetch();
     }
+
+    function ListarPorBISenha($n_bi, $senha){
+        $con = GetConexao();
+        $sql = "select * from administrador where n_bi_administrador = ? and senha_administrador = md5(?)";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue( 1, $n_bi);
+        $stmt->bindValue( 2, $senha);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    function ListarPorIDSenha($id_administrador, $senha){
+        $con = GetConexao();
+        $sql = "select * from administrador where id_administrador = ? and senha_administrador = md5(?)";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue( 1, $id_administrador);
+        $stmt->bindValue( 2, $senha);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    function ListarPorBI($n_bi){
+        $con = GetConexao();
+        $sql = "select * from administrador where n_bi_administrador = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue( 1, $n_bi);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+    
 
 }
--- app/dao/publicador_dao.php
+++ app/dao/publicador_dao.php
@@ -4,7 +4,7 @@ class PublicadorDao implements Crud {
     
     function Cadastrar($publicador){
         $con = GetConexao();
-        $sql = "insert into publicador (nome_publicador, sobrenome_publicador, email_publicador, senha_publicador, nascimento_publicador, genero_publicador, n_bi_publicador) values (?,?,?,?,?,?,?);";
+        $sql = "insert into publicador (nome_publicador, sobrenome_publicador, email_publicador, senha_publicador, nascimento_publicador, genero_publicador, n_bi_publicador) values (?, ?, ?, md5(?), ?, ?, ?);";
         $stmt = $con->prepare($sql);
         $stmt->bindValue( 1, $publicador->GetNome());
         $stmt->bindValue( 2, $publicador->GetSobrenome());
@@ -18,16 +18,24 @@ class PublicadorDao implements Crud {
 
     function Actualizar($publicador){
         $con = GetConexao();
-        $sql = "update publicador set nome_publicador = ?, sobrenome_publicador = ?, email_publicador = ?, senha_publicador = ?, nascimento_publicador = ?, genero_publicador = ?, n_bi_publicador = ? where id_publicador = ?;";
+        $sql = "update publicador set nome_publicador = ?, sobrenome_publicador = ?, email_publicador = ?, nascimento_publicador = ?, genero_publicador = ?, n_bi_publicador = ? where id_publicador = ?;";
         $stmt = $con->prepare($sql);
         $stmt->bindValue( 1, $publicador->GetNome());
         $stmt->bindValue( 2, $publicador->GetSobrenome());
         $stmt->bindValue( 3, $publicador->GetEmail());
-        $stmt->bindValue( 4, $publicador->GetSenha());
-        $stmt->bindValue( 5, $publicador->GetNascimento());
-        $stmt->bindValue( 6, $publicador->GetGenero());
-        $stmt->bindValue( 7, $publicador->GetN_bi());
-        $stmt->bindValue( 8, $publicador->GetId());
+        $stmt->bindValue( 4, $publicador->GetNascimento());
+        $stmt->bindValue( 5, $publicador->GetGenero());
+        $stmt->bindValue( 6, $publicador->GetN_bi());
+        $stmt->bindValue( 7, $publicador->GetId());
+        return $stmt->execute();
+    }
+
+    function AlterarSenha($id_publicador, $senha_nova){
+        $con = GetConexao();
+        $sql = "update publicador set senha_publicador = md5(?) where id_publicador = ?;";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue( 1, $senha_nova);
+        $stmt->bindValue( 2, $id_publicador);
         return $stmt->execute();
     }
 
@@ -43,7 +51,8 @@ class PublicadorDao implements Crud {
         $con = GetConexao();
         $sql = "select * from publicador;";
         $stmt = $con->prepare($sql);
-        return $stmt->execute();
+        $stmt->execute();
+        return $stmt->fetchAll();
     }
 
     function ListarPorId($id){
@@ -51,7 +60,38 @@ class PublicadorDao implements Crud {
         $sql = "select * from publicador where id_publicador = ?;";
         $stmt = $con->prepare($sql);
         $stmt->bindValue( 1, $id);
-        return $stmt->execute();
+        $stmt->execute();
+        return $stmt->fetch();
     }
+
+    function ListarPorBISenha($n_bi, $senha){
+        $con = GetConexao();
+        $sql = "select * from publicador where n_bi_publicador = ? and senha_publicador = md5(?)";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue( 1, $n_bi);
+        $stmt->bindValue( 2, $senha);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    function ListarPorIDSenha($id_publicador, $senha){
+        $con = GetConexao();
+        $sql = "select * from publicador where id_publicador = ? and senha_publicador = md5(?)";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue( 1, $id_publicador);
+        $stmt->bindValue( 2, $senha);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+
+    function ListarPorBI($n_bi){
+        $con = GetConexao();
+        $sql = "select * from publicador where n_bi_publicador = ?";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue( 1, $n_bi);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+    
 
 }
