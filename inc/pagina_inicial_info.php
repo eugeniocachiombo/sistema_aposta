6dfc1867c65fe572a1a423733d80be9d00b44a24
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-24 21:00:30 +0100

    Retificando a palavra a respeito

--- inc/pagina_inicial_info.php
+++ inc/pagina_inicial_info.php
@@ -9,7 +9,7 @@
         <?php if($acesso == "administrador" || $acesso == "gestor" || $acesso == "publicador"){ ?>
 
         <div class="row pt-4">
-            <p>Registos a respecto de cadastro de equipas e partidas</p>
+            <p>Registos a respeito de cadastro de equipas e partidas</p>
             <div class="col-12 col-sm-6 ">
                 <div class="table-responsive" style="max-height: 60vh">
                     <table class="table table-warning table-striped table-hover">
@@ -74,7 +74,7 @@
         <?php } ?>
 
         <div class="row pt-4">
-            <p>Registos a respecto de partidas publicadas</p>
+            <p>Registos a respeito de partidas publicadas</p>
             <div class="col ">
                 <div class="table-responsive" style="max-height: 60vh">
                     <table class="table table-info table-striped table-hover">
@@ -134,42 +134,42 @@
         <hr style="color: #299" size="8px" class=''>
 
         <div class="row pt-4">
-            <p>Registos a respecto de apostas e resultados obtidos nas sessões</p>
+            <p>Registos a respeito de apostas e resultados obtidos nas sessões</p>
             
             <?php if( $acesso == "administrador" ){ ?>
 
-            <div class="col-12 col-sm-6 ">
-                <div class="table-responsive" style="max-height: 60vh">
-                    <table class="table table-primary table-striped table-hover">
-                        <tr style="border: none;">
-                            <th class="text-start">Apostas</th>
-                        </tr>
+                <div class="col-12 col-sm-6 ">
+                    <div class="table-responsive" style="max-height: 60vh">
+                        <table class="table table-primary table-striped table-hover">
+                            <tr style="border: none;">
+                                <th class="text-start">Apostas</th>
+                            </tr>
 
-                        <?php 
-                            $aposta_dao = new ApostaDao();
-                            $retorno = $aposta_dao->Listar();
-                            foreach ($retorno as $value) {
+                            <?php 
+                                $aposta_dao = new ApostaDao();
+                                $retorno = $aposta_dao->Listar();
+                                foreach ($retorno as $value) {
 
-                                $data_aposta = explode("-", $value['data_aposta']);
-                                $data_aposta = $data_aposta[2] . "/" . $data_aposta[1] . "/" . $data_aposta[0];
+                                    $data_aposta = explode("-", $value['data_aposta']);
+                                    $data_aposta = $data_aposta[2] . "/" . $data_aposta[1] . "/" . $data_aposta[0];
 
-                                $hora_aposta = str_split(strval($value['hora_aposta']), 5);
-                                $hora_aposta = $hora_aposta[0];
+                                    $hora_aposta = str_split(strval($value['hora_aposta']), 5);
+                                    $hora_aposta = $hora_aposta[0];
 
-                               echo "<tr style='border: none;'>" .
-                                        "<td style='border: none;text-align:left'> " 
-                                            . "Apostador: <b>" . $value['nome_apostador'] . " " . $value['sobrenome_apostador'] . 
-                                            "</b> <br> Equivalência: <b>" . $value['nome_equipaA'] . " " . $value['golos_equipaA'] ." - " . " " . $value['golos_equipaB'] . " " . $value['nome_equipaB'] . 
-                                            "</b><br>  Data de aposta: <b>". $data_aposta . " " . $hora_aposta . "</b>" .
-                                            "</b> <br>  Quantia Apostada: <b>". $value['valor_apostado'] . " 00,KZ</b>" .
-                                        "</td>" .
-                                     "</tr>";
-                            }
-                        ?>
-                    </table>
+                                echo "<tr style='border: none;'>" .
+                                            "<td style='border: none;text-align:left'> " 
+                                                . "Apostador: <b>" . $value['nome_apostador'] . " " . $value['sobrenome_apostador'] . 
+                                                "</b> <br> Equivalência: <b>" . $value['nome_equipaA'] . " " . $value['golos_equipaA'] ." - " . " " . $value['golos_equipaB'] . " " . $value['nome_equipaB'] . 
+                                                "</b><br>  Data de aposta: <b>". $data_aposta . " " . $hora_aposta . "</b>" .
+                                                "</b> <br>  Quantia Apostada: <b>". $value['valor_apostado'] . " 00,KZ</b>" .
+                                            "</td>" .
+                                        "</tr>";
+                                }
+                            ?>
+                        </table>
+                    </div>
+                    <hr class=" d-sm-none" size="6px">
                 </div>
-                <hr class=" d-sm-none" size="6px">
-            </div>
 
             <?php } ?>
 
