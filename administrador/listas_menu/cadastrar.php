1e44c715472d8e1eadfa3cd192260adcf8ea938b
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-29 22:17:25 +0100

    Sistema iconizado

--- administrador/listas_menu/cadastrar.php
+++ administrador/listas_menu/cadastrar.php
@@ -1,7 +1,7 @@
 <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="../inicio">Página Inicial</a>
+    <a class="nav-link active" aria-current="page" href="../inicio"><i class="fas fa-home"></i> Página Inicial</a>
 </li>
 
 <li class="nav-item">
-    <a class="nav-link" href="autenticar.php">Autenticar</a>
+    <a class="nav-link" href="autenticar.php"><i class="fas fa-user"></i> Autenticar</a>
 </li>
--- lista_principal_menu/index.php
+++ lista_principal_menu/index.php
@@ -3,15 +3,15 @@
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
         aria-expanded="false">
-        Equipa
+        <i class="fas fa-users"></i> Equipa
     </a>
     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
         <?php if( $acesso == "gestor" || $acesso == "administrador" ){ ?>
-        <li><a class="dropdown-item" href="../equipa/cadastrar.php">Cadastrar</a></li>
-        <li><a class="dropdown-item" href="../equipa/actualizar.php">Actualizar</a></li>
-        <li><a class="dropdown-item" href="../equipa/eliminar.php">Eliminar</a></li>
+        <li><a class="dropdown-item" href="../equipa/cadastrar.php"><i class="fas fa-plus"></i> Cadastrar</a></li>
+        <li><a class="dropdown-item" href="../equipa/actualizar.php"><i class="fas fa-refresh"></i> Actualizar</a></li>
+        <li><a class="dropdown-item" href="../equipa/eliminar.php"><i class="fas fa-trash"></i> Eliminar</a></li>
         <?php } ?>
-        <li><a class="dropdown-item" href="../equipa/listar.php">Listar</a></li>
+        <li><a class="dropdown-item" href="../equipa/listar.php"><i class="fas fa-list"></i> Listar</a></li>
     </ul>
 </li>
 
@@ -20,16 +20,16 @@
     <?php if( $acesso != "apostador" ){ ?>
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
         aria-expanded="false">
-        Partida
+        <i class="fas fa-futbol"></i> Partida
     </a>
 
     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
         <?php if( $acesso == "gestor" || $acesso == "administrador" ){ ?>
-        <li><a class="dropdown-item" href="../partida/cadastrar.php">Criar</a></li>
-        <li><a class="dropdown-item" href="../partida/actualizar.php">Actualizar</a></li>
-        <li><a class="dropdown-item" href="../partida/eliminar.php">Eliminar</a></li>
+        <li><a class="dropdown-item" href="../partida/cadastrar.php"><i class="fas fa-plus"></i> Criar</a></li>
+        <li><a class="dropdown-item" href="../partida/actualizar.php"><i class="fas fa-refresh"></i> Actualizar</a></li>
+        <li><a class="dropdown-item" href="../partida/eliminar.php"><i class="fas fa-trash"></i> Eliminar</a></li>
         <?php } ?>
-        <li><a class="dropdown-item" href="../partida/listar.php">Listar</a></li>
+        <li><a class="dropdown-item" href="../partida/listar.php"><i class="fas fa-list"></i> Listar</a></li>
     </ul>
     <?php } ?>
 </li>
@@ -37,16 +37,16 @@
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle txt_publicar" href="#" id="navbarDropdown" role="button"
         data-bs-toggle="dropdown" aria-expanded="false">
-        Partidas Publicadas
+        <i class="fas fa-flag"></i> Partidas Publicadas
     </a>
 
     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
         <?php if( $acesso == "publicador" || $acesso == "administrador" ){ ?>
-        <li><a class="dropdown-item " href="../partida_publicada/cadastrar.php">Publicar </a></li>
-        <li><a class="dropdown-item " href="../partida_publicada/actualizar.php">Actualizar </a></li>
-        <li><a class="dropdown-item " href="../partida_publicada/eliminar.php">Eliminar </a></li>
+        <li><a class="dropdown-item " href="../partida_publicada/cadastrar.php"><i class="fas fa-flag"></i> Publicar </a></li>
+        <li><a class="dropdown-item " href="../partida_publicada/actualizar.php"><i class="fas fa-refresh"></i> Actualizar </a></li>
+        <li><a class="dropdown-item " href="../partida_publicada/eliminar.php"><i class="fas fa-trash"></i> Eliminar </a></li>
         <?php } ?>
-        <li><a class="dropdown-item " href="../partida_publicada/listar.php">Listar</a></li>
+        <li><a class="dropdown-item " href="../partida_publicada/listar.php"><i class="fas fa-list"></i> Listar</a></li>
     </ul>
 </li>
 
@@ -55,18 +55,18 @@
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
         aria-expanded="false">
-        Aposta
+        <i class="fas fa-dollar-sign"></i> Aposta
     </a>
 
     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
         <?php if( $acesso == "apostador"  ){ ?>
-            <li><a class="dropdown-item" href="../aposta/cadastrar.php">Apostar</a></li>
-            <li><a class="dropdown-item" href="../aposta/minhas_apostas.php">Minhas Apostas</a></li>
-            <li><a class="dropdown-item" href="../aposta/eliminar.php">Eliminar</a></li>
+            <li><a class="dropdown-item" href="../aposta/cadastrar.php"><i class="fas fa-sack-dollar"></i> Apostar</a></li>
+            <li><a class="dropdown-item" href="../aposta/minhas_apostas.php"><i class="fas fa-scale-unbalanced"></i> Minhas Apostas</a></li>
+            <li><a class="dropdown-item" href="../aposta/eliminar.php"><i class="fas fa-trash"></i> Eliminar</a></li>
         <?php } ?>
 
         <?php if( $acesso == "administrador" ){ ?>
-            <li><a class="dropdown-item" href="../aposta/listar.php">Listar</a></li>
+            <li><a class="dropdown-item" href="../aposta/listar.php"><i class="fas fa-list"></i> Listar</a></li>
         <?php } ?>
     </ul>
 </li>
@@ -75,16 +75,16 @@
 <li class="nav-item dropdown" >
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
         aria-expanded="false">
-        Resultados
+        <i class="fas fa-flag-checkered"></i> Resultados
     </a>
 
     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
         <?php if( $acesso == "publicador" || $acesso == "administrador" ){ ?>
-                <li><a class="dropdown-item" href="../resultado_publicado/cadastrar.php">Publicar</a></li>
-                <li><a class="dropdown-item" href="../resultado_publicado/actualizar.php">Actualizar</a></li>
-                <li><a class="dropdown-item" href="../resultado_publicado/eliminar.php">Eliminar</a></li>
+                <li><a class="dropdown-item" href="../resultado_publicado/cadastrar.php"><i class="fas fa-flag"></i> Publicar</a></li>
+                <li><a class="dropdown-item" href="../resultado_publicado/actualizar.php"><i class="fas fa-refresh"></i> Actualizar</a></li>
+                <li><a class="dropdown-item" href="../resultado_publicado/eliminar.php"><i class="fas fa-trash"></i> Eliminar</a></li>
         <?php } ?>
-        <li><a class="dropdown-item" href="../resultado_publicado/listar.php">Listar</a></li>
+        <li><a class="dropdown-item" href="../resultado_publicado/listar.php"><i class="fas fa-list"></i> Listar</a></li>
     </ul>
 </li>
 
@@ -92,21 +92,21 @@
 <li class="nav-item dropdown" >
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
         aria-expanded="false">
-        Vitórias
+        <i class="fas fa-medal"></i> Vitórias
     </a>
 
     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
         <?php if( $acesso == "apostador"  ){ ?>
-            <li><a class="dropdown-item" href="../apostador/minhas_vitorias.php">Minhas vitórias</a></li>
+            <li><a class="dropdown-item" href="../apostador/minhas_vitorias.php"><i class="fas fa-trophy"></i> Minhas vitórias</a></li>
         <?php } ?>
 
         <?php if( $acesso == "administrador" ){ ?>
-            <li><a class="dropdown-item" href="../administrador/vencedores.php">Vencedores</a></li>
+            <li><a class="dropdown-item" href="../administrador/vencedores.php"><i class="fas fa-trophy"></i> Vencedores</a></li>
         <?php } ?>
     </ul>
 </li>
 <?php } ?>
 
 <li class="nav-item" style="min-width: 160px">
-    <a class="nav-link" href="../terminar_sessao">Terminar Sessão</a>
+    <a class="nav-link" href="../terminar_sessao"><i class="fas fa-right-from-bracket"></i> Terminar Sessão</a>
 </li>
